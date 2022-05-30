b688743b53f6b82c95025c1fd81c39e3c108cad9
janikvonrotz <contact@janikvonrotz.ch>
2022-05-30 10:48:33 +0200

    lint

--- project_task_dependency_counter/__manifest__.py
+++ project_task_dependency_counter/__manifest__.py
@@ -1,23 +1,17 @@
 {
-    'name': "Project Task Dependency Counter",
-
-    'summary': """
-        Module summary.
+    "name": "Project Task Dependency Counter",
+    "summary": """
+        Adds smart button with dependency counter to task from.
     """,
-    
-    'author': 'Mint System GmbH, Odoo Community Association (OCA)',
-    'website': 'https://www.mint-system.ch',
-    'category': 'Project',
-    'version': '13.0.1.0.0',
-    'license': 'AGPL-3',
-    
-    'depends': ['project_task_dependency'],
-
-    'data': [
-        'views/project_task_view.xml',
-    ],
-
-    'installable': True,
-    'application': False,
-    'auto_install': False,
+    "author": "Mint System GmbH, Odoo Community Association (OCA)",
+    "website": "https://www.mint-system.ch",
+    "category": "Project",
+    "version": "13.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["project_task_dependency"],
+    "data": ["views/project_task_view.xml"],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+    "images": ["images/screen.png"],
 }
