320a69031bc825dda094eb32e8a780de70fb1e9f
Janik von Rotz <login@janikvonrotz.ch>
2023-05-09 12:02:24 +0200

    [project_phase] init

---
+++ project_phase/__manifest__.py
@@ -0,0 +1,23 @@
+{
+    "name": "Project Phase",
+    "summary": """
+        Manage project task by project phases.
+    """,
+    "author": "Mint System GmbH, Odoo Community Association (OCA)",
+    "website": "https://www.mint-system.ch",
+    "category": "Tools",
+    "version": "15.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["project"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/project_phase.xml",
+        "views/project_task.xml",
+        "views/project_project.xml",
+    ],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+    "images": ["images/screen.png"],
+    "qweb": ["static/src/xml/board.xml"],
+}
