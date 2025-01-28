52ebc87e00a07a75c5eb6c69accd8f60cc109580
Janik von Rotz <login@janikvonrotz.ch>
2025-01-28 18:05:37 +0100

    chore: update author

--- project_phase_estimate/__manifest__.py
+++ project_phase_estimate/__manifest__.py
@@ -3,7 +3,7 @@
     "summary": """
         Estimate planned hours by project and phases.
     """,
-    "author": "Mint System GmbH, Odoo Community Association (OCA)",
+    "author": "Mint System GmbH",
     "website": "https://www.mint-system.ch",
     "category": "Tools",
     "version": "16.0.1.0.0",
