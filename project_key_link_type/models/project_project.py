c8f5385e8fcbb3e792a895b81b6f68b79359b25e
Janik von Rotz <login@janikvonrotz.ch>
2025-11-18 22:38:16 +0100

    refactor(project_key_link_type): update dependencies and project key generation logic
    - Add `project_template` to module dependencies
    - Modify `type_id` field with `inverse` method to generate project keys
    - Remove redundant key generation logic from `write` and `create` methods
    - Disable `_onchange_project_name` method
    - Adjust form view to group `type_id` field under `user_id`

--- project_key_link_type/models/project_project.py
+++ project_key_link_type/models/project_project.py
@@ -3,61 +3,24 @@ import logging
 from odoo import api, fields, models
 
 _logger = logging.getLogger(__name__)
-from odoo.osv.expression import AND, OR
 
 
 class Project(models.Model):
     _inherit = "project.project"
 
-    type_id = fields.Many2one(copy=True)
-    key = fields.Char(
-        string="Project Key",
-        required=True,
-        default="/",
-        copy=False,
-    )
+    type_id = fields.Many2one(copy=True, inverse="_inverse_type_id")
 
-    def _set_key(self, vals):
+    def _inverse_type_id(self):
         """
         Create project sequence from type.
         """
-        type_id = self.type_id or self.env["project.type"].browse(vals.get("type_id"))
-        if type_id:
-            vals["key"] = type_id.sequence_id.next_by_id()
-        return vals
+        for project in self:
+            if project.type_id and not project.is_template:
+                project.key = project.type_id.sequence_id.next_by_id()
 
-    def write(self, vals):
+    @api.onchange("name")
+    def _onchange_project_name(self):
         """
-        Generate key if default is set.
+        Disable this onchange method.
         """
-        if ((vals.get("key") or self.key) == "/") and not self.is_template:
-            self._set_key(vals)
-        res = super().write(vals)
-
-        # Update analytic account
-        self._update_analytic_account()
-        return res
-
-    @api.model
-    def create(self, vals):
-        """
-        Generate key if default is set.
-        """
-        if vals.get("key", "/") == "/":
-            self._set_key(vals)
-        
-        # Setup and update analytic account
-        analytic_account = self._create_analytic_account_from_values(vals)
-        vals["analytic_account_id"] = analytic_account.id
-        res = super().create(vals)
-        res._update_analytic_account()
-        return res
-
-    def _update_analytic_account(self):
-        for project in self.filtered(lambda p: p.analytic_account_id):
-            project.analytic_account_id.write(
-                {
-                    "name": "[%s] %s" % (project.key, project.name),
-                    "partner_id": project.partner_id.id,
-                }
-            )
+        return False
