37e9d9d8431ad01e2b3b06444a7434c962e4b314
Janik von Rotz <login@janikvonrotz.ch>
2026-03-05 15:51:19 +0100

    feat(project_phase_estimate): set default allow_estimate to True for new projects
    - Set `allow_estimate` field to default True to enable estimate functionality by default
    - Maintain existing help text while ensuring new projects have estimates enabled out of the box

--- project_phase_estimate/models/project_project.py
+++ project_phase_estimate/models/project_project.py
@@ -8,5 +8,5 @@ _logger = logging.getLogger(__name__)
 class ProjectProject(models.Model):
     _inherit = "project.project"
 
-    allow_estimate = fields.Boolean(help="Add estimates linked to phases to this project.")
+    allow_estimate = fields.Boolean(default=True, help="Add estimates linked to phases to this project.")
     estimate_ids = fields.One2many("project.estimate", "project_id")
